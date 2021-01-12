2fbbfc320c946d750789246ba3f7d508da73ec2a
Cesar alves <ceaser.alves03@gmail.com>
2021-01-12 15:25:59 -0300

    Implementação da classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.sql.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,7 +12,18 @@ public class Program {
 		Department obj = new Department(1,"books");
 		System.out.println(obj);
 		
+		
+		Seller seller = new Seller(21 , "bob" , "bob@gmail.com" , 
+		 new Date(0, 0, 0), 3000.00 , obj);
 
+		
+		System.out.println(seller);
+		
+		
+		
+		
+		
+		
 	}
 
 }
