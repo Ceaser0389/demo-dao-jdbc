c15d29c9b5fc865033592a4371d3edb52bd5dcd9
César Alves <ceaser.alves03@gmaill.com>
2024-05-04 17:42:27 -0300

    Implementando delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,7 +14,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		
+		Scanner sc = new Scanner(System.in);
 		
 		
 		
@@ -56,10 +57,13 @@ public class Program {
 		System.out.println("Update Complete");
 		
 		
+		System.out.println("\n=== TEST 6: seller delete ====");
+		System.out.println("Enter id for delete id:");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
 		
-		
-		
-		
+		sc.close();
 		
 	}
 }
